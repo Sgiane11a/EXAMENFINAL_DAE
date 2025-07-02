4a206d604706fdbd98047a98526f9e14c8144ef3
Sgiane11a <steyci.cordova@tecsup.edu.pe>
2025-07-02 15:49:28 -0500

    Examen Final Finalizado

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,4 @@
 import { useState } from 'react'
-import './App.css'
 import Header from './components/Header'
 import { Route, Routes } from 'react-router-dom'
 import { Listado } from './pages/Listado'
